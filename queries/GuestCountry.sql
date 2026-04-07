9a395f2bd3dd8a4d169f06b877446abc6219c00e
Alifia Ganjaraharja <121094831+alifiaganjaraharja@users.noreply.github.com>
2026-04-07 16:17:32 +0700

    Delete queries/GuestCountry.sql

--- queries/GuestCountry.sql
+++
@@ -1,7 +0,0 @@
-# Find out the most to least guest countries for this hotel
-SELECT
-Guest_Country,
-COUNT(Guest_Country) AS Total_Guest
-FROM hoteldata-455008.KaggleHotel.Hotel_01
-GROUP BY Guest_Country
-ORDER BY Total_Guest DESC;
