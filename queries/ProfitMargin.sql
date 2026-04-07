188c4f6a2d45d4f1b25ff3978e6ff1a8df0839cb
Alifia Ganjaraharja <121094831+alifiaganjaraharja@users.noreply.github.com>
2026-04-07 14:57:18 +0700

    Delete queries/ProfitMargin.sql

--- queries/ProfitMargin.sql
+++
@@ -1,9 +0,0 @@
-# Query to analyze the profit margin of this hotel every month
-SELECT 
-    Month,
-    SUM(Total_Revenue) AS Total_Revenue,
-    SUM(Total_Costs) AS Total_Costs,
-    ROUND((SUM(Total_Revenue) - SUM(Total_Costs)) / SUM(Total_Revenue) * 100,2) AS Profit_Margin
-FROM hoteldata-455008.KaggleHotel.Hotel_01
-GROUP BY Month
-ORDER BY Profit_Margin DESC;
