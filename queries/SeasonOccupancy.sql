851ffe3d6628fd7114cb6488bcc52f04d8eb5063
Alifia Ganjaraharja <121094831+alifiaganjaraharja@users.noreply.github.com>
2026-04-07 15:44:31 +0700

    Delete queries/SeasonOccupancy.sql

--- queries/SeasonOccupancy.sql
+++
@@ -1,9 +0,0 @@
-# A query to analyze the occupancy rate by Season of this hotel
-SELECT 
-    Season, 
-    ROUND(AVG(Occupancy_Rate) * 100,2) AS Avg_Occupancy_Rate,
-    ROUND(AVG(Occupancy_Managed_Guests),2) AS Avg_Occupancy_Managed,
-    ROUND(AVG(Occupancy_All) * 100.2) AS Avg_Occupancy_All
-FROM hoteldata-455008.KaggleHotel.Hotel_01
-GROUP BY Season
-ORDER BY Avg_Occupancy_Rate DESC;
