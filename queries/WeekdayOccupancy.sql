0a24023615fb489fe0b49205bd8bd15061fcd012
Alifia Ganjaraharja <121094831+alifiaganjaraharja@users.noreply.github.com>
2026-04-07 15:44:57 +0700

    Delete queries/WeekdayOccupancy.sql

--- queries/WeekdayOccupancy.sql
+++
@@ -1,9 +0,0 @@
-# A query to analyze the occupancy rate by Weekday of this hotel
-SELECT 
-    Weekday, 
-    ROUND(AVG(Occupancy_Rate) * 100,2) AS Avg_Occupancy_Rate,
-    ROUND(AVG(Occupancy_Managed_Guests),2) AS Avg_Occupancy_Managed,
-    ROUND(AVG(Occupancy_All) * 100,2) AS Avg_Occupancy_All
-FROM hoteldata-455008.KaggleHotel.Hotel_01
-GROUP BY Weekday
-ORDER BY Avg_Occupancy_Rate DESC;
