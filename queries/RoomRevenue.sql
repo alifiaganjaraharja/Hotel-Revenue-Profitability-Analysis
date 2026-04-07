2fe1af7fa398dc340cb220da3843a1382cfa3bfe
Alifia Ganjaraharja <121094831+alifiaganjaraharja@users.noreply.github.com>
2026-04-07 14:57:31 +0700

    Delete queries/RoomRevenue.sql

--- queries/RoomRevenue.sql
+++
@@ -1,9 +0,0 @@
-# Analyze the Room Revenue Contribution of this hotel every month
-SELECT 
-    Month,
-    SUM(Room_Revenue) AS Total_Room_Revenue,
-    SUM(Total_Revenue) AS Total_Revenue,
-    ROUND((SUM(Room_Revenue) / SUM(Total_Revenue)) * 100,2) AS Room_Revenue_Contribution
-FROM hoteldata-455008.KaggleHotel.Hotel_01
-GROUP BY Month
-ORDER BY Room_Revenue_Contribution DESC;
