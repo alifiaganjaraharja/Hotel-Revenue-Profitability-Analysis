769ca254012c00ea6f9fd8b9bbbf5be8acca06a2
Alifia Ganjaraharja <121094831+alifiaganjaraharja@users.noreply.github.com>
2026-04-07 15:46:56 +0700

    Delete queries/ADR&RevPAR.sql

--- queries/ADR&RevPAR.sql
+++
@@ -1,9 +0,0 @@
-# A query to track and analyze ADR & RevPAR trends across different seasons
-SELECT 
-    Season, 
-    DATE_TRUNC(Date, MONTH) AS Month,  -- Truncate to the first day of the month
-    ROUND(AVG(ADR),2) AS Avg_ADR, 
-    ROUND(AVG(RevPAR),2) AS Avg_RevPAR
-FROM hoteldata-455008.KaggleHotel.Hotel_01
-GROUP BY Season, Month
-ORDER BY Month ASC;
