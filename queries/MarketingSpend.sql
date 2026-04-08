5d463d8a31dfe074790a1de786b52dd57d9059a4
Alifia Ganjaraharja <121094831+alifiaganjaraharja@users.noreply.github.com>
2026-04-08 17:26:02 +0700

    Delete queries/MarketingSpend.sql

--- queries/MarketingSpend.sql
+++
@@ -1,9 +0,0 @@
-# Analyze the effectiveness of the Marketing spend of this hotel every month
-SELECT 
-    Month,
-    SUM(Total_Revenue) AS Total_Revenue,
-    SUM(Marketing_Spend) AS Total_Marketing_Spend,
-    ROUND(SUM(Total_Revenue) / NULLIF(SUM(Marketing_Spend), 0),2) * 100 AS Revenue_per_Marketing_Spend
-FROM hoteldata-455008.KaggleHotel.Hotel_01
-GROUP BY Month
-ORDER BY Revenue_per_Marketing_Spend DESC;
